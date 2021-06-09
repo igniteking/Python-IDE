2c6e2186cf55798f655d5f3e20c48370d6b3dd74
igniteking <khanzaidan786@gmail.com>
2021-06-09 14:40:23 +0530

    new

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 
-
+<?php include_once("database/phpmyadmin/connection.php"); ?>
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
